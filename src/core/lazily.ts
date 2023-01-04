20f935e68167d3a5adc81fe3929652298713164b
JLarky <jlarky@gmail.com>
2023-01-03 17:58:25 -0700

    try civet
    fswatch src/core/lazily.civet | while read; do civet < src/core/lazily.civet > src/core/lazily.ts; done

--- src/core/lazily.ts
+++ src/core/lazily.ts
@@ -1,18 +1,19 @@
 import { lazy } from 'react'
 
-export const lazily = <T extends {}, U extends keyof T>(
+export function lazily <T extends {}, U extends keyof T>(
   loader: (x?: string) => Promise<T>
-) => {
-  return new Proxy({} as unknown as T, {
-    get: (target, componentName: string | symbol) => {
+) {
+  return new Proxy(({} as unknown) as T, {
+    get: function(target, componentName: string | symbol) { 
       if (typeof componentName === 'string') {
         return lazy(() => {
-          return loader(componentName).then((x) => ({
-            default: x[componentName as U] as any as React.ComponentType<any>,
-          }))
+          return loader(componentName).then((x) => {
+            return {default: (x[componentName as U] as any) as React.ComponentType<any>}
+          }
+         )
         })
       }
       return
-    },
-  })
+    }
+    })
 }
