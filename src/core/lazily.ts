05812dc4699a69b82b1afd5b1f4e1cddf5b45046
JLarky <jlarky@gmail.com>
2023-01-03 17:26:50 -0700

    add returns

--- src/core/lazily.ts
+++ src/core/lazily.ts
@@ -2,15 +2,17 @@ import { lazy } from 'react'
 
 export const lazily = <T extends {}, U extends keyof T>(
   loader: (x?: string) => Promise<T>
-) =>
-  new Proxy(({} as unknown) as T, {
+) => {
+  return new Proxy({} as unknown as T, {
     get: (target, componentName: string | symbol) => {
       if (typeof componentName === 'string') {
-        return lazy(() =>
-          loader(componentName).then((x) => ({
-            default: (x[componentName as U] as any) as React.ComponentType<any>,
+        return lazy(() => {
+          return loader(componentName).then((x) => ({
+            default: x[componentName as U] as any as React.ComponentType<any>,
           }))
-        )
+        })
       }
+      return
     },
   })
+}
