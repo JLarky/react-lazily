f7df974e0e4c8e0417845fca80e787cb52c16a4e
ColorFilter <ismedusa@gmail.com>
2023-09-15 20:04:11 +0900

    refactor: Simplify and enhance `lazily` function
    1. Introduced descriptive type aliases for better code readability.
    2. Simplified loader function signature to be more specific.
    3. Added error handling for component lookup failures.
    4. Improved type checking for returned components.

--- src/core/lazily.ts
+++ src/core/lazily.ts
@@ -1,16 +1,19 @@
 import { lazy } from 'react'
 
-export const lazily = <T extends {}, U extends keyof T>(
-  loader: (x?: string) => Promise<T>
-) =>
-  new Proxy(({} as unknown) as T, {
-    get: (target, componentName: string | symbol) => {
-      if (typeof componentName === 'string') {
-        return lazy(() =>
-          loader(componentName).then((x) => ({
-            default: (x[componentName as U] as any) as React.ComponentType<any>,
-          }))
-        )
-      }
+type ComponentName = string;
+type Loader<T> = () => Promise<T>;
+type NamedComponents = Record<ComponentName, unknown>;
+
+export const lazily = <T extends NamedComponents>(loader: Loader<T>) => {
+  return new Proxy({} as T, {
+    get: (_target, name: ComponentName) => {
+      return lazy(async () => {
+        const module = await loader();
+        const Component = module[name] as ComponentType<T>;
+        if (!Component) throw new Error(`Component ${name} not found`);
+
+        return { default: Component };
+      });
     },
-  })
+  });
+};
